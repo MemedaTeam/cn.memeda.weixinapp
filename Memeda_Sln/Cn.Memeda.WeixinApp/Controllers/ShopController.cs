2048a3e0994f7918c673fb8472bc7dc72c15b6d2
xiaofoyuan <295211024@qq.com>
2015-06-23 23:42:26 +0800

    1、修改登录界面以及相关UI布局 2、增加店铺首页以及相关内容

---
+++ Memeda_Sln/Cn.Memeda.WeixinApp/Controllers/ShopController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cn.Memeda.WeixinApp.Controllers
+{
+    public class ShopController : Controller
+    {
+        //
+        // GET: /Shop/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
