e4a9efb188f3f452b14493ff7674735040149ce5
tomkluas <1175704775@qq.com>
2015-07-04 21:10:23 +0800

    调整js

--- Memeda_Sln/Cn.Memeda.WeixinApp/Scripts/Index.js
+++ Memeda_Sln/Cn.Memeda.WeixinApp/Scripts/Index.js
@@ -7,6 +7,7 @@
         this.merchantsList = option.hasOwnProperty("merchantsList") && option.merchantsList || "";
         this.productlist = option.hasOwnProperty("productlist") && option.productlist || "";
         this.orderList = option.hasOwnProperty("orderList") && option.orderList || "";
+        this.orderitemurl = option.hasOwnProperty("orderitemurl") && option.orderitemurl || "";
     };
     indexPage.prototype = {
         getIndexCommunity: function () {
@@ -34,12 +35,12 @@
                 }
             });
         },
-        getCommunitityList: function () {
+        getCommunitityList: function (sn) {
             var that = this;
             $.ajax({
                 method: method,
                 url: that.communtityListUrl,
-                data: null,
+                data: { "sn": sn },
                 success: function (data) {
                     var list = { "content": data };
                     var html = template("locationitem", list);
@@ -60,13 +61,12 @@
                 }
             });
         },
-        getAllOrderList: function (parentEle,paymentStatus, shippingStatus) {
+        getAllOrderList: function (parentEle, paymentStatus, shippingStatus) {
             var that = this;
             $.ajax({
                 method: method,
                 url: that.orderList,
-                headers: { "tokenM": "N193YW5na2U1OQ==" },
-                data: { "paymentStatus": paymentStatus, "shippingStatus": shippingStatus },
+                data: { "tokenM": "N193YW5na2U1OQ==", "paymentStatus": paymentStatus, "shippingStatus": shippingStatus },
                 success: function (data) {
                     console.log(data);
                     var html = template("orderitem", data.content);
@@ -74,38 +74,86 @@
                 }
             });
         },
-
-        ReisterEvent: function () {
+        getOrderItemInfo: function () {
             var that = this;
+            $.ajax({
+                method: method,
+                url: that.orderitemurl,
+                data: { "sn": this.GetParameter("sn") },
+                success: function (data) {
+                    var list = { "content": data };
+                    var html = template("orderItems", list);
+                    $("#sendtime").before(html);
+                }
+            });
+        },
+
+        RegisterIndex: function () {
+            var that = this, loadfruit = 0, loadsnack = 0, loadmarket = 0;
+            $(".product_menu li a").click(function () {
+                var attrThis = $(this).attr("tip");
+                $(".product_menu li a").removeClass("now_product");
+                $(this).addClass("now_product");
+                $(attrThis).show().siblings().hide();
+            });
+
             if ($("#fruiticon").length > 0) {
                 $("#fruiticon").click(function () {
-                    that.getProductList(82, $("#fruitlist"));
+                    if (loadfruit == 0) {
+                        that.getProductList(82, $("#fruitlist"));
+                    }
+                    loadfruit = 1;
                 });
             }
             if ($("#snackicon").length > 0) {
                 $("#snackicon").click(function () {
-                    that.getProductList(83, $("#snacklist"));
+                    if (loadsnack == 0) {
+                        that.getProductList(83, $("#snacklist"));
+                    }
+                    loadsnack = 1;
                 });
             }
             if ($("#marketicon").length > 0) {
                 $("#marketicon").click(function () {
-                    that.getProductList(84, $("#marketlist"));
+                    if (loadmarket == 0) {
+                        that.getProductList(84, $("#marketlist"));
+                    }
+                    loadmarket = 1;
                 });
             }
-            if ($("#snacklist").length > 0) {
-                $("#snacklist").click(function () {
-                    that.getProductList(83, $("#snacklist"));
-                });
-            }
-            if ($("#marketlist").length > 0) {
-                $("#marketlist").click(function () {
-                    that.getProductList(84, $("#marketlist"));
-                });
-            }
-        },
-        init: function () {
-            this.ReisterEvent();
             this.getIndexCommunity();
+        },
+        RegisterOrder: function () {
+            var that = this;
+            var tabs = $(".order_tab a"), divs = $("#order_change").children("li");
+            for (var p = 0; p < tabs.length; p++) {
+                tabs[p].onclick = function () { change(this); };
+            }
+            function change(obj) {
+                for (var i = 0; i < tabs.length; i++) {
+                    if (tabs[i] == obj) {
+                        tabs[i].className = "order_click";
+                        divs[i].style.display = "block";
+                    }
+                    else {
+                        tabs[i].className = "";
+                        divs[i].style.display = "none";
+                        $(this).parent(".kemu_member_tab").css("background", "url(images/member_06.jpg) no-repeat 0 0");
+                    }
+                }
+            }
+
+            that.getAllOrderList($("#allorder"));
+        },
+        RegisterOrderInfo: function () {
+            this.getOrderItemInfo();
+        },
+        GetParameter: function (pName) {
+            var reg = new RegExp("(^|&)" + pName + "=([^&]*)(&|$)", "i");
+            var r = window.location.search.substr(1).match(reg);
+            if (r != null)
+                return unescape(r[2]);
+            return "";
         }
     };
     window.IndexPage = indexPage;
