a8b1a52057f1b5d2eaaf23490e9f495778ff7f45
xiaofoyuan <295211024@qq.com>
2015-07-11 16:13:00 +0800

    1、提取店铺相关处理函数为公共函数 2、修改购物车的相关操作

--- Memeda_Sln/Cn.Memeda.WeixinApp/Scripts/common.js
+++ Memeda_Sln/Cn.Memeda.WeixinApp/Scripts/common.js
@@ -1,7 +1,50 @@
 // JavaScript Document
 $(document).ready(function (e) {
-    SetCookie("openid","123456",7);
+    SetCookie("openid", "123456", 7);
+
+    //进入购物车
+    $("#btn-goto-car").click(function () {
+        GoToCar();
+    });
 });
+//绑定添加商品到购物车的事件
+function BindAddToCarEvent()
+{
+    //减少商品
+    $(".count_odd").click(function () {
+        var product_account = parseInt($(".now_count").text());
+        var carGoodsCount = parseInt($("#btn-goto-car>span").html());
+        if (product_account >= 1) {
+            product_account = product_account - 1;
+            carGoodsCount = carGoodsCount - 1;
+            $(".now_count").text(product_account)
+            $(".car_shop span").text(carGoodsCount);
+        }
+        else {
+            product_account = 0;
+            $(".now_count").text(product_account);
+            $(".count_group").css("z-index", "1");
+            $(".button_add_car").css("z-index", "2");
+        }
+        car_span();
+    });
+    //增加商品
+    $(".count_add").click(function () {
+        var product_account = parseInt($(".now_count").text());
+        var carGoodsCount = parseInt($("#btn-goto-car>span").html());
+        product_account = product_account + 1;
+        carGoodsCount = carGoodsCount + 1;
+        $(".now_count").text(product_account)
+        $(".car_shop span").text(carGoodsCount);
+        car_span();
+    });
+    //增加商品到购物车
+    $("#btn-add-to-car").click(function () {
+        var id = $(this).attr("value");
+        var product_account = parseInt($(".now_count").text());
+        var data = AddToCar(id, product_account);
+    });
+}
 //验证手机号码
 function ValidPhone(phoneNumber) {
     if (phoneNumber == undefined || phoneNumber == '') {
@@ -14,6 +57,24 @@ function ValidPhone(phoneNumber) {
         return false;
     }
     return true;
+}
+
+//获取购物车商品数量
+function GetCarGoodsCount() {
+    $.ajax({
+        type: "get",
+        url: " http://120.24.228.51:8080/20150623/weixin/cart/quantity.jhtml",
+        data: { openid: GetOpenid() },
+        dataType: "json",
+        crossDomain: true,
+        beforeSend: function () { },
+        success: function (data) {
+            if (data != null && data.quantity > 0)
+                $(".car_shop span").text(data.quantity);
+        },
+        error: function () { },
+        complete: function () { }
+    });
 }
 //设置Cookie
 function SetCookie(c_name, value, expiredays) {
@@ -59,6 +120,111 @@ function GetRequest() {
     }
     return theRequest;
 }
+//获取openid
+function GetOpenid() {
+    return GetCookie("openid");
+}
+//加载店铺商品
+function LoadShopGoodsList(merchantsId, cataID) {
+    $.ajax({
+        type: "Get",
+        url: " http://120.24.228.51:8080/20150623/weixin/product/list/" + cataID + ".jhtml",
+        data: { merchantsId: merchantsId, openid: GetOpenid() },
+        dataType: "json",
+        crossDomain: true,
+        beforeSend: function () { },
+        success: function (data) {
+            if (data != null && data.content != null) {
+                LoadGoodsList(data.content);                
+            }
+            else { loginErrorEle.html(data.content); }
+        },
+        error: function () { },
+        complete: function () { }
+    });
+}
+//加载商品列表
+function LoadGoodsList(goodsList) {
+    var productList = '';
+    $.each(goodsList, function (i, item) {
+        productList += '<li>';
+        productList += '<a href="#" value="' + item.id + '">';
+        productList += '<img src="' + item.thumbnail + '" class="fruit_img" url="' + goodsList.image + '">';
+        productList += '<p><span class="fr"><i class="weight">' + item.weight + '</i>/' + item.unit + '</span><i>' + item.name + '</i></p>';
+        productList += '<div class="product_price"><img src="/Content/images/car_07.jpg" /><i>￥' + item.price + '</i><span>￥' + item.price + '</span></div>';
+        productList += '</a>';
+        productList += '</li>';
+        if (i == goodsList.length - 1) {
+            //加载元素
+            var strShopGoodsListEle = $('#shop-goods-list').html().replace('<div class="clear"></div>', '');
+            strShopGoodsListEle += productList;
+            strShopGoodsListEle += '<div class="clear"></div>';
+            $('#shop-goods-list').html(strShopGoodsListEle);
+
+            //商品信息弹出层
+            var goodsListEle = $('#shop-goods-list>li>a');
+            BindGoodsListEvent(goodsListEle);
+            BindAddToCarEvent();
+        }
+    });
+}
+//绑定商品列表元素的点击事件
+function BindGoodsListEvent(goodsListEle) {
+    //商品信息弹出层
+    goodsListEle.click(function () {
+        $(".now_count").html("0");
+        //改变图片
+        var urls = $(this).children(".fruit_img").attr("url");
+        var href2 = '<img  src="' + urls + '" />';
+        $(".product_picture").html(href2);
+        /*改变店铺名*/
+        $(".shop_name span").text($("#shop-shopkeeper").text());
+        /*商品名称与价格*/
+        var product_names = $(this).children("p").children("i").text();
+        $(".product_name i").text(product_names);
+        var product_pric = $(this).children(".product_price").children("i").text();
+        $(".product_name span").text(product_pric);
+        /*商品规格*/
+        $(".weight span").text($(this).children("p").children("span").children(".weight").text());
+        ShowGoodsDetail($(this).attr("value"));
+    });
+    ClearThirdBorderRight(goodsListEle);
+    CloseGoodsDetail();
+}
+//三列布局，去除最后一列div的右边框
+function ClearThirdBorderRight(goodsListEle) {
+    var shop_product_N = goodsListEle
+    var n = shop_product_N.length;
+    for (i = 0; i < n; i++) {
+        var index_now = shop_product_N.index(shop_product_N[i]) + 1;
+        if (index_now % 3 == 0) {
+            shop_product_N[i].style.borderRight = "1px solid #ffffff"
+        }
+    }
+}
+/*显示商品详情*/
+function ShowGoodsDetail(id) {
+    var product_alert = parseInt($(".product_detail").height());
+    var margin_top = product_alert / 2 + 20;
+    $(".product_detail").css({ "top": "20%", "margin-top": -margin_top });
+    $("#btn-add-to-car").attr("value", id);
+    $(".fixed").show();
+    $(".product_detail").show();
+}
+//关闭商品详情弹出页
+function CloseGoodsDetail()
+{
+    //关闭弹出层
+    $(".close_detail").click(function () {
+        HidePop();
+    })
+}
+//隐藏商品详情弹出框
+function HidePop() {
+
+    $(".fixed").hide();
+    $(".product_detail").hide();
+}
 //添加商品到购物车
 function AddToCar(id, quantity) {
     var ele = $('#car-error-message');
@@ -77,14 +243,21 @@ function AddToCar(id, quantity) {
         beforeSend: function () { },
         success: function (data) {
             ele.html(data.content);
+            HidePop();
         },
         error: function () { },
         complete: function () { }
     });
 }
-//获取openid
-function GetOpenid() {
-    return GetCookie("openid");
+/*购物车图标*/
+function car_span(carEle) {
+    var car_span = $(".car_shop span").text();
+    if (car_span > 0) {
+        $(".car_shop span").show()
+    }
+    else {
+        $(".car_shop span").hide()
+    }
 }
 //进入购物车
 function GoToCar()
--- Memeda_Sln/Cn.Memeda.WeixinApp/Scripts/shop/shop.js
+++ Memeda_Sln/Cn.Memeda.WeixinApp/Scripts/shop/shop.js
@@ -4,75 +4,11 @@ $(document).ready(function (e) {
     var request = new Object();
     request = GetRequest();
     var cataID = request["cataid"];
-    var merchantsId = request["merchantsId"];
+    var merchantsId = request["merchantsid"];
     LoadShopInfomation(merchantsId);
     LoadShopGoodsList(merchantsId, cataID);
     GetCarGoodsCount();
-    //购物车
-    $("#btn-goto-car").click(function () {
-        //$(this).css("z-index", "1");
-        //$(".count_group").css("z-index", "2");
-        GoToCar();
-    });
-    var product_account = parseInt($(".now_count").text());
-    //减少商品
-    $(".count_odd").click(function () {
-        if (product_account > 1) {
-            product_account = product_account - 1;
-            $(".now_count").text(product_account)
-            $(".car_shop span").text(product_account);
-            $(".trolley_count span").text(product_account);
-        }
-        else {
-            product_account = 0;
-            $(".now_count").text(product_account);
-            $(".car_shop span").text(product_account);
-            $(".count_group").css("z-index", "1");
-            $(".button_add_car").css("z-index", "2");
-        }
-        car_span();
-    });
-    //增加商品
-    $(".count_add").click(function () {
-        product_account = product_account + 1;
-        $(".now_count").text(product_account)
-        $(".car_shop span").text(product_account);
-        $(".trolley_count span").text(product_account);
-        car_span();
-    });
-    //增加商品到购物车
-    $("#btn-add-to-car").click(function () {
-        var id = $(this).attr("value");
-        var data = AddToCar(id, product_account);
-    });
 });
-//获取购物车商品数量
-function GetCarGoodsCount() {
-    $.ajax({
-        type: "get",
-        url: " http://120.24.228.51:8080/20150623/weixin/cart/quantity.jhtml",
-        data: { openid: GetOpenid() },
-        dataType: "json",
-        crossDomain: true,
-        beforeSend: function () { },
-        success: function (data) {
-            if (data != null && data.quantity > 0)
-                $(".car_shop span").text(data.quantity);
-        },
-        error: function () { },
-        complete: function () { }
-    });
-}
-/*购物车图标*/
-function car_span() {
-    var car_span = $(".car_shop span").text();
-    if (car_span > 0) {
-        $(".car_shop span").show()
-    }
-    else {
-        $(".car_shop span").hide()
-    }
-}
 //加载店铺信息
 function LoadShopInfomation(merchantsId) {
     $.ajax({
@@ -96,86 +32,4 @@ function LoadShopInfomation(merchantsId) {
         error: function () { },
         complete: function () { }
     });
-}
-//加载店铺商品
-function LoadShopGoodsList(merchantsId,cataID) {
-    $.ajax({
-        type: "Get",
-        url: " http://120.24.228.51:8080/20150623/weixin/product/list/"+cataID+".jhtml",
-        data: { merchantsId: merchantsId,openid:GetOpenid() },
-        dataType: "json",
-        crossDomain: true,
-        beforeSend: function () { },
-        success: function (data) {
-            if (data != null && data.content != null) {
-                var productList = '';
-                $.each(data.content, function (i, item) {
-                    productList += '<li>';
-                    productList += '<a href="#" value="'+item.id+'">';
-                    productList += '<img src="' + item.thumbnail + '" class="fruit_img" url="' + data.image + '">';
-                    productList += '<p><span class="fr"><i class="weight">' + item.weight + '</i>/' + item.unit + '</span><i>' + item.name + '</i></p>';
-                    productList += '<div class="product_price"><img src="/Content/images/car_07.jpg" /><i>￥' + item.price + '</i><span>￥' + item.price + '</span></div>';
-                    productList += '</a>';
-                    productList += '</li>';
-                    if (i == data.content.length - 1) {
-                        //加载元素
-                        var strShopGoodsListEle = $('#shop-goods-list').html().replace('<div class="clear"></div>', '');
-                        strShopGoodsListEle += productList;
-                        strShopGoodsListEle += '<div class="clear"></div>';
-                        $('#shop-goods-list').html(strShopGoodsListEle);
-
-                        //商品信息弹出层
-                        $('#shop-goods-list>li>a').click(function () {
-
-                            //改变图片
-                            var urls = $(this).children(".fruit_img").attr("url");
-                            var href2 = '<img  src="' + urls + '" />';
-                            $(".product_picture").html(href2);
-                            /*改变店铺名*/
-                            $(".shop_name span").text($("#shop-shopkeeper").text());
-                            /*商品名称与价格*/
-                            var product_names = $(this).children("p").children("i").text();
-                            $(".product_name i").text(product_names);
-                            var product_pric = $(this).children(".product_price").children("i").text();
-                            $(".product_name span").text(product_pric);
-                            /*商品规格*/
-                            $(".weight span").text($(this).children("p").children("span").children(".weight").text());
-                            ShowGoodsDetail($(this).attr("value"));
-                        });
-                        ClearThirdBorderRight();
-                        //关闭弹出层
-                        $(".close_detail").click(function () {
-                            $(".fixed").hide();
-                            $(".product_detail").hide();
-
-                        })
-                    }
-                });
-            }
-            else { loginErrorEle.html(data.content); }
-        },
-        error: function () { },
-        complete: function () { }
-    });
-}
-
-/*超市第3个块 去除右边线*/
-function ClearThirdBorderRight() {
-    var shop_product_N = $(".producter_con_shop li a")
-    var n = shop_product_N.length;
-    for (i = 0; i < n; i++) {
-        var index_now = shop_product_N.index(shop_product_N[i]) + 1;
-        if (index_now % 3 == 0) {
-            shop_product_N[i].style.borderRight = "1px solid #ffffff"
-        }
-    }
-}
-/*显示商品详情*/
-function ShowGoodsDetail(id) {
-    var product_alert = parseInt($(".product_detail").height());
-    var margin_top = product_alert / 2 + 20;
-    $(".product_detail").css({ "top": "20%", "margin-top": -margin_top });
-    $("#btn-add-to-car").attr("value",id);
-    $(".fixed").show();
-    $(".product_detail").show();
 }
