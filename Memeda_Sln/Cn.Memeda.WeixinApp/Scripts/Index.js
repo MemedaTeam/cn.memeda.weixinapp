68e0e6d72001c6234c4cd9e416da90f6c98efc5f
tomkluas <1175704775@qq.com>
2015-07-12 09:34:21 +0800

    支付调整

--- Memeda_Sln/Cn.Memeda.WeixinApp/Scripts/Index.js
+++ Memeda_Sln/Cn.Memeda.WeixinApp/Scripts/Index.js
@@ -199,14 +199,39 @@
             });
         },
         getPay: function () {
-            this.innerAjax(this.payurl, { "type": "payment", "paymentPluginId": "WeixinpayPlugin", "sn": "20150630914", "amount": 100 }, function (data) {
-
+            var that = this;
+            this.innerAjax(this.payurl, { "type": "payment", "paymentPluginId": "weixinpayPlugin", "sn": that.getParameter("sn") }, function (data) {
+                function onBridgeReady() {
+                    WeixinJSBridge.invoke(
+                        'getBrandWCPayRequest', {
+                            "appId": "wx0e475aceab8cf432",     //公众号名称，由商户传入     
+                            "timeStamp": data.timestamp,         //时间戳，自1970年以来的秒数     
+                            "nonceStr": data.nonceStr, //随机串     
+                            "package": data.package,
+                            "signType": data.signType,         //微信签名方式:     
+                            "paySign": data.paySign //微信签名 
+                        },
+                        function (res) {
+                            if (res.err_msg == "get_brand_wcpay_request:ok") { }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+                        }
+                    );
+                }
+                if (typeof WeixinJSBridge == "undefined") {
+                    if (document.addEventListener) {
+                        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+                    } else if (document.attachEvent) {
+                        document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+                        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+                    }
+                } else {
+                    onBridgeReady();
+                }
             }, "post");
         },
         innerAjax: function (url, data, callback, mtd) {
             data = data || {};
             mtd = mtd || method;
-            data.openid = '123456';
+            data.openid = GetOpenid();
             $.ajax({
                 method: mtd,
                 url: url,
@@ -386,3 +411,4 @@
     };
     window.IndexPage = indexPage;
 })();
+
