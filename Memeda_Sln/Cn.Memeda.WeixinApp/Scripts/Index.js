81c0f6ee26296defb1f4674b5704284fcda7aefa
tomkluas <1175704775@qq.com>
2015-07-20 18:44:15 +0800

    正式版调整

--- Memeda_Sln/Cn.Memeda.WeixinApp/Scripts/Index.js
+++ Memeda_Sln/Cn.Memeda.WeixinApp/Scripts/Index.js
@@ -280,8 +280,8 @@
                             "paySign": data.paySign //微信签名 
                         },
                         function (res) {
-                            WeixinJSBridge.log(res.err_msg);
-                            alert(res.err_code + res.err_desc + res.err_msg);
+                            //WeixinJSBridge.log(res.err_msg);
+                            //alert(res.err_code + res.err_desc + res.err_msg);
                             if (res.err_msg == "get_brand_wcpay_request:ok") {
                                 location.href = "/order/";
                             }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
@@ -315,15 +315,6 @@
                     callback(result);
                 }
             });
-        },
-        caricon: function car_span(carEle) { /*购物车图标*/
-            var car_span = $(".car_shop span").text();
-            if (car_span > 0) {
-                $(".car_shop span").show()
-            }
-            else {
-                $(".car_shop span").hide()
-            }
         },
 
         RegisterLocation: function () {
